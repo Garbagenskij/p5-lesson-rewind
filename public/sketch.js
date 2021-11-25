df935f9809b7ae56f70d2b40640b87dff918a16f
garbagenskij <gretacozza99@gmail.com>
2021-11-25 17:27:29 +0100

    added socket

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,17 @@
+//(5>) aggiungo questo blocco
+let clientSocket = io();
+clientSocket.on("connect", newConnection);
+function newConnection() {
+  console.log(clientSocket.id);
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  //(>4) per vedere che faccia qualcosa aggiungo questo
+  //set up e draw vengono fuori in automatico, standard come in ogni p5 project
   circle(mouseX, mouseY, 20);
 }
--- server.js
+++ server.js
@@ -13,6 +13,17 @@ let server = app.listen(port);
 //alla fine check (avrei potuto consolloggare qualsiasi cosa, così è piu raffinato e mi checka pure la variabile)
 console.log("Server is running on http://localhost:" + port);
 
-//(3>)
+//(>3)
 //app.use(express.static("nome del file"));
 app.use(express.static("public"));
+
+// (>4)
+//dichiariamo la library come prima
+let serverSocket = require("socket.io");
+//creiamo un oggetto che gestisce i messaggi input (i) e output(o)
+//server è la variabile dichiarat prima
+let io = serverSocket(server);
+io.on("connection", newConnection);
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
