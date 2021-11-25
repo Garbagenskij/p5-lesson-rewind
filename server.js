4dca842b0293fc80245c0c8e1ec97ca0ed062224
garbagenskij <gretacozza99@gmail.com>
2021-11-25 18:44:22 +0100

    added Procfile

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ let express = require("express");
 // stiamo caricando la libraru dentro una variabile
 // ora creiamo l'applicazione: praticamante duchiariamo una variabile dentro alla quale express runna
 let app = express();
-//creiamo poi un port
-let port = 3000;
-//e alla fine un server containing the running istance of our server
+
+//->(>11)  lo commento per non leggerlo perchè ora faccip diversamnte
+
+//creiamo poi un port...
+//let port = 3000;
+
+//(>11)
+// praticamente ho Sostituisco a port = 3000 questa riga di codice "prebuilt"
+//che prende il port di Heroku.
+// Gli do anche un alternativa: con double pipeline gli sto dicendo: se non stai runnando su Heroku, usa 3000
+let port = process.env.PORT || 3000;
+
+//...e alla fine un server containing the running istance of our server
 let server = app.listen(port);
 //alla fine check (avrei potuto consolloggare qualsiasi cosa, così è piu raffinato e mi checka pure la variabile)
 console.log("Server is running on http://localhost:" + port);
